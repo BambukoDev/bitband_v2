c41e7b787cf28ba1370af12d3b0eeda11eb44af3
BambukoDev <bambukodev@gmail.com>
2026-03-10 19:56:36 +0100

    NO

--- src/bin/services/wifi.rs
+++ src/bin/services/wifi.rs
@@ -1,3 +1,4 @@
+use bt_hci::cmd::info;
 use embassy_time::{Duration, Timer};
 use esp_hal_dhcp_server::{simple_leaser::SingleDhcpLeaser, structs::DhcpServerConfig};
 use esp_radio::wifi::{AccessPointConfig, ClientConfig, Config as RadioConfig, ModeConfig, Protocol, WifiController, WifiDevice, WifiMode as RadioWifiMode};
@@ -26,13 +27,13 @@ pub async fn wifi_ap_task(
     controller: &'static mut WifiController<'static>,
     stack: &'static Stack<'static>,
 ) {
-    controller.set_mode(RadioWifiMode::ApSta);
     loop {
         let mode = WIFI_MODE_SIGNAL.wait().await;
 
         match mode {
             WifiMode::Ap => {
                 let _ = controller.stop();
+                // let _ = controller.stop();
                 IS_STA.store(false, core::sync::atomic::Ordering::SeqCst);
 
                 let config = StaticConfigV4 {
@@ -49,12 +50,19 @@ pub async fn wifi_ap_task(
                     .with_auth_method(esp_radio::wifi::AuthMethod::Wpa2Personal);
                 controller.set_config(&ModeConfig::AccessPoint(ap_config)).unwrap();
                 controller.start().unwrap();
-                info!("AP Started");
+
+                for i in 1..=5 {
+                    if stack.is_link_up() {
+                        info!("AP Started");
+                    }
+                    Timer::after_millis(500).await;
+                }
             }
             WifiMode::Sta(ssid, pass) => {
+                // controller.set_mode(RadioWifiMode::Sta).unwrap();
                 let _ = controller.stop();
                 IS_STA.store(true, core::sync::atomic::Ordering::SeqCst);
-                info!("Connecting to {} with password {}", ssid.as_str(), pass.as_str());
+                info!("Connecting to {}", ssid.as_str());
                 // let config = StaticConfigV4 {
                 //     address: embassy_net::Ipv4Cidr::new(embassy_net::Ipv4Address::new(192, 168, 1, 200), 24),
                 //     gateway: Some(embassy_net::Ipv4Address::new(192, 168, 4, 1)),
@@ -63,9 +71,7 @@ pub async fn wifi_ap_task(
 
                 let mut config = DhcpConfig::default();
                 config.hostname = core::convert::TryInto::try_into("BitBandV2").ok();
-                info!("Is config up: {}", stack.is_config_up());
                 stack.set_config_v4(embassy_net::ConfigV4::Dhcp(config));
-                info!("Is config up: {}", stack.is_config_up());
 
                 let client_config = ClientConfig::default()
                     .with_ssid(ssid.to_string())
@@ -101,6 +107,7 @@ pub async fn wifi_ap_task(
             WifiMode::Disabled => {
                 info!("Stopped WiFi");
                 let _ = controller.stop();
+                controller.set_config(&ModeConfig::None).unwrap();
                 IS_STA.store(false, core::sync::atomic::Ordering::SeqCst);
             }
         }
@@ -112,7 +119,9 @@ pub async fn wifi_ap_task(
 #[embassy_executor::task]
 pub async fn dhcp_server_task(stack: &'static Stack<'static>) {
     loop {
-        if stack.is_link_up() && !IS_STA.load(core::sync::atomic::Ordering::SeqCst) {
+        info!("Stack Link: {}", stack.is_link_up());
+        info!("Condif: {}", stack.is_config_up());
+        if stack.is_link_up() /* && !IS_STA.load(core::sync::atomic::Ordering::SeqCst) */ {
             if let Some(cfg) = stack.config_v4() {
                 if cfg.address.address() == Ipv4Address::new(192, 168, 4, 1) {
                     let config = DhcpServerConfig {
